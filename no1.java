58e66ff035faf807f0b0aed17a2dd97be253a02e
adrielrakaa <114383364+adrielrakaa@users.noreply.github.com>
2022-09-26 14:28:53 +0700

    Add files via upload

---
+++ no1.java
@@ -0,0 +1,14 @@
+// Raka Adriel 
+//20210040137
+
+package Sesi1;
+
+public class no1 {
+    public static void main(String[] args) {
+        
+        System.out.println("Bejalar JAVA");
+        System.out.println("Sangat mudah sekali");
+        System.out.println("Dan sangat menyenangkan");
+
+    }
+}
